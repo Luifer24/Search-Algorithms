b42fbd3e79dccb8e797b7681f058e23950fa53f2
Luifer Úbeda <60629312+Luifer24@users.noreply.github.com>
2020-08-27 19:03:34 +0200

    Update binary_searc_sqrt.py
    Introduction of binary search

--- binary_searc_sqrt.py
+++ binary_searc_sqrt.py
@@ -1,3 +1,6 @@
+# We use the algorithm binary search to solve a problem of the square root of a number.
+# The square root is not perfect, it can have decimals 
+
 target = int(input('Put you number: '))
 epsilon = 0.01
 down = 0.0
